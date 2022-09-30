b5b99e4a7cc1b877680fa2424bf7f263f51d4c99
Revchenko Dmitry <reuchankadzmitry@gmail.com>
2022-09-30 18:44:22 +0300

    feat: install styles-components and define the global styles

---
+++ src/styles/global.ts
@@ -0,0 +1,17 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+    *{
+        margin: 0;
+        padding: 0;
+        outline: 0;
+        box-sizing: border-box;
+    }
+    a{
+        text-decoration: none;
+        color: inherit;
+    }
+    button{
+        font: inherit;
+    }
+`;
