d9cdb2e003c82a758c6d6c7faa539385b0e4c2d4
Revchenko Dmitry <reuchankadzmitry@gmail.com>
2022-10-01 14:15:37 +0300

    feat: add types for assets response

---
+++ src/store/assets.model.ts
@@ -0,0 +1,19 @@
+export type AssetType = {
+  id: string;
+  rank: string;
+  symbol: string;
+  name: string;
+  supply: string;
+  maxSupply: string;
+  marketCapUsd: string;
+  volumeUsd24Hr: string;
+  priceUsd: string;
+  changePercent24Hr: string;
+  vwap24Hr: string;
+  explorer: string;
+};
+
+export type AssetsType = {
+  data: AssetType[]
+  timestamp: number;
+};
