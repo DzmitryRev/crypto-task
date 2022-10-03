05c5652b241a4f5c11012e1084259f00fdf6af25
Revchenko Dmitry <reuchankadzmitry@gmail.com>
2022-10-03 20:26:40 +0300

    feat: add get and set logic of localStorage

--- src/services/localStorage.service.ts
+++ src/services/localStorage.service.ts
@@ -1,14 +1,28 @@
-class PortfolioStorage {
-  static getPortfolio() {
+import { AssetType } from '../store/assets.model';
 
+type StorageAssetType = {
+  asset: AssetType;
+  total: number;
+};
+
+class PortfolioStorage {
+  static storageName: string = 'crypto-curr';
+
+  static getPortfolio() {
+    const storageData: StorageAssetType[] = JSON.parse(
+      localStorage.getItem(this.storageName) || '[]',
+    );
+    return storageData;
   }
 
-  static addToPortfolio() {
-
+  static addToPortfolio(asset: AssetType, total: number) {
+    const newAsset: StorageAssetType = { asset, total };
+    const storageData: StorageAssetType[] = this.getPortfolio();
+    storageData.push(newAsset);
+    localStorage.setItem(this.storageName, JSON.stringify(storageData));
   }
 
   static removeFromPortfolio() {
-
   }
 }
 
