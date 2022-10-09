cde9b4017e9cf57a5d25dfc98a5ec041d79e1882
Revchenko Dmitry <reuchankadzmitry@gmail.com>
2022-10-09 21:38:36 +0300

    feat: add unit tests for Modal component

---
+++ src/components/Modal.test.js
@@ -0,0 +1,42 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable no-undef */
+/* eslint-disable react/react-in-jsx-scope */
+/* eslint-disable react/jsx-filename-extension */
+import { fireEvent, render, screen } from '@testing-library/react';
+import 'jest-styled-components';
+import Modal from './Modal';
+
+test('Modal close test', () => {
+  const closeCallback = jest.fn();
+  render(
+    <Modal closeCallback={() => { closeCallback(); }}>Hi</Modal>,
+  );
+  const modalShadow = screen.getByTestId('modal-shadow');
+  const modal = screen.getByTestId('modal');
+  const modalClose = screen.getByTestId('modal-close-btn');
+  expect(modalShadow).toBeInTheDocument();
+  fireEvent.click(modalShadow);
+  fireEvent.click(modalClose);
+  // Not called from modal click, call only from shadow or close btn
+  fireEvent.click(modal);
+  expect(closeCallback).not.toBeCalledTimes(3);
+  expect(closeCallback).toBeCalledTimes(2);
+});
+
+test('Modal children prop test', () => {
+  render(
+    <Modal>Hello</Modal>,
+  );
+  const modal = screen.getByTestId('modal');
+  expect(modal).toBeInTheDocument();
+  expect(modal).toHaveTextContent('Hello');
+});
+
+test('Modal isOpen prop test', () => {
+  render(
+    <Modal isOpen={false}>Hello</Modal>,
+  );
+  const modal = screen.getByTestId('modal');
+  expect(modal).toBeInTheDocument();
+  expect(modal).not.toBeVisible();
+});
--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -12,8 +12,13 @@ function Modal({ isOpen, closeCallback, children }: PropsWithChildren<ModalProps
   const { closeModal } = useModal(isOpen, closeCallback);
   return (
     <>
-      <StyledModalContainer aria-hidden={isOpen} onClick={closeModal} />
+      <StyledModalContainer
+        data-testid="modal-shadow"
+        aria-hidden={isOpen}
+        onClick={closeModal}
+      />
       <StyledModal
+        data-testid="modal"
         aria-hidden={isOpen}
         onClick={(e) => {
           e.stopPropagation();
@@ -21,7 +26,12 @@ function Modal({ isOpen, closeCallback, children }: PropsWithChildren<ModalProps
       >
         {children}
         <div className="close-btn">
-          <ClearIcon fontSize="large" color="inherit" onClick={closeModal} />
+          <ClearIcon
+            data-testid="modal-close-btn"
+            fontSize="large"
+            color="inherit"
+            onClick={closeModal}
+          />
         </div>
       </StyledModal>
     </>
