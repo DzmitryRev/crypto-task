bb7a70acf637562495cfbadc079dc6f5aea52f48
Revchenko Dmitry <reuchankadzmitry@gmail.com>
2022-10-16 16:52:43 +0300

    feat: test implement trpc

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,58 +1,79 @@
-import React from 'react';
-import { Route, Routes } from 'react-router-dom';
-import Header from './components/Header/Header';
-import Modal from './components/Modal/Modal';
-import Asset from './pages/Asset';
-import Main from './pages/Main';
-import Portfolio from './pages/Portfolio';
-import BuyAsset from './pages/BuyAsset';
-import { StyledWrapper } from './styles';
+import React, { useState } from 'react';
+// import { Route, Routes } from 'react-router-dom';
+import { createReactQueryHooks } from '@trpc/react';
+import { AppRouter } from 'server';
+import { ReactQueryDevtools } from 'react-query/devtools';
+// import Header from './components/Header/Header';
+// import Modal from './components/Modal/Modal';
+// import Asset from './pages/Asset';
+// import Main from './pages/Main';
+// import Portfolio from './pages/Portfolio';
+// import BuyAsset from './pages/BuyAsset';
+// import { StyledWrapper } from './styles';
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+export const trpc = createReactQueryHooks<AppRouter>();
+
+function AppContent() {
+  const hello = trpc.useQuery(['hello']);
+  return <main className="p-2">{hello.data?.name}</main>;
+}
 
 function App() {
+  const [queryClient] = useState(() => new QueryClient());
+  const [trpcClient] = useState(() => trpc.createClient({
+    url: 'http://localhost:4000/trpc',
+  }));
   return (
-    <StyledWrapper>
-      <Header />
-      <div>
-        <Routes>
-          <Route path="/" element={<Main />}>
-            <Route
-              path="portfolio"
-              element={(
-                <Modal type="default">
-                  <Portfolio />
-                </Modal>
-              )}
-            />
-            <Route
-              path="buy/:assetId"
-              element={(
-                <Modal type="minify">
-                  <BuyAsset />
-                </Modal>
-              )}
-            />
-          </Route>
-          <Route path="/asset/:assetId" element={<Asset />}>
-            <Route
-              path="buy/:assetId"
-              element={(
-                <Modal type="minify">
-                  <BuyAsset />
-                </Modal>
-              )}
-            />
-            <Route
-              path="portfolio"
-              element={(
-                <Modal type="default">
-                  <Portfolio />
-                </Modal>
-              )}
-            />
-          </Route>
-        </Routes>
-      </div>
-    </StyledWrapper>
+    <trpc.Provider client={trpcClient} queryClient={queryClient}>
+      <QueryClientProvider client={queryClient}>
+        <AppContent />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
+    </trpc.Provider>
+    // <StyledWrapper>
+    //   <Header />
+    //   <div>
+    //     <Routes>
+    //       <Route path="/" element={<Main />}>
+    //         <Route
+    //           path="portfolio"
+    //           element={(
+    //             <Modal type="default">
+    //               <Portfolio />
+    //             </Modal>
+    //           )}
+    //         />
+    //         <Route
+    //           path="buy/:assetId"
+    //           element={(
+    //             <Modal type="minify">
+    //               <BuyAsset />
+    //             </Modal>
+    //           )}
+    //         />
+    //       </Route>
+    //       <Route path="/asset/:assetId" element={<Asset />}>
+    //         <Route
+    //           path="buy/:assetId"
+    //           element={(
+    //             <Modal type="minify">
+    //               <BuyAsset />
+    //             </Modal>
+    //           )}
+    //         />
+    //         <Route
+    //           path="portfolio"
+    //           element={(
+    //             <Modal type="default">
+    //               <Portfolio />
+    //             </Modal>
+    //           )}
+    //         />
+    //       </Route>
+    //     </Routes>
+    //   </div>
+    // </StyledWrapper>
   );
 }
 
--- server/server.ts
+++ server/server.ts
@@ -1,19 +1,37 @@
 // import * as trpc from "@trpc/server";
 import express, { Express, Request, Response } from "express";
-import dotenv from "dotenv";
+import * as trpc from "@trpc/server";
+import { z } from "zod";
+import * as trpcExpress from "@trpc/server/adapters/express";
+import cors from "cors";
 
-dotenv.config();
+const createContext = ({ req, res }: trpcExpress.CreateExpressContextOptions) => ({ req, res });
+
+export type Context = trpc.inferAsyncReturnType<typeof createContext>;
+
+function createRouter() {
+    return trpc.router<Context>();
+  }
+
+const appRouter = createRouter().query("hello", {
+  async resolve() {
+    return { name: "dima" };
+  },
+});
+export type AppRouter = typeof appRouter;
 
 const app: Express = express();
 
-app.get("/hello", (req: Request, res: Response) => {
-  res.send("Hello world");
-});
+app.use(cors());
 
-app.get("/", (req: Request, res: Response) => {
-    res.send("Main page");
-  });
+app.use(
+  "/trpc",
+  trpcExpress.createExpressMiddleware({
+    router: appRouter,
+    createContext,
+  })
+);
 
-app.listen(3000, () => {
+app.listen(4000, () => {
   console.log("Server is running");
 });
